dd1c23194ef5e87b6f38635e54a760eeb9b8b32f
noahchaney214 <117609324+noahchaney214@users.noreply.github.com>
2022-11-06 17:26:56 -0800

    Added 3 main files and started to divide files into respective directories.

---
+++ Python Scripts/Main Files/main.py
@@ -0,0 +1,41 @@
+import settings as game
+import pygame as pg
+import numpy as np
+import level
+
+x = np.abs(-12)
+
+print(x)
+
+# initialize pygame
+pg.init()
+
+# creating the canvas/screen
+screen = pg.display.set_mode(game.size)
+pg.display.set_caption("Ray Caster 3D")
+
+exit = False
+
+currentLevel = 0
+
+
+print(level.LEVEL[currentLevel])
+
+currentLevel += 1
+
+print(level.LEVEL[currentLevel])
+
+while not exit:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit = True
+
+    pg.display.update()
+
+
+
+
+
+if __name__ == '__main__':
+    print("Hello")
---
+++ Python Scripts/Settings/settings.py
@@ -0,0 +1,12 @@
+
+# some basic colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0 ,0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+# screen size
+WIDTH = 1280
+HEIGHT = 720
+size = (WIDTH, HEIGHT)
